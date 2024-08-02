9bbf859453a03924f98b15c43754f9eb708ce16d
Rocio Woscoff <rociowoscoff3@gmail.com>
2024-08-02 00:51:36 -0300

    Actualizar índice después de limpiar caché

--- generate_password.py
+++
@@ -1,9 +0,0 @@
-import bcrypt
-
-password = 'admin' 
-
-# Genera el hash de la contraseña
-hashed = bcrypt.hashpw(password.encode('utf-8'), bcrypt.gensalt())
-
-# Imprime el hash en la consola
-print(hashed.decode('utf-8'))
