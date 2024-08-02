189200c840c8daeda0e775cf6886b820f0346c17
Rocio Woscoff <rociowoscoff3@gmail.com>
2024-08-02 15:01:47 -0300

    arreglos

---
+++ static/script.js
@@ -0,0 +1,13 @@
+function togglePasswordVisibility(passwordFieldId) {
+    var passwordField = document.getElementById(passwordFieldId);
+    var icon = passwordField.nextElementSibling.querySelector('i');
+    if (passwordField.type === "password") {
+        passwordField.type = "text";
+        icon.classList.remove('fa-eye');
+        icon.classList.add('fa-eye-slash');
+    } else {
+        passwordField.type = "password";
+        icon.classList.remove('fa-eye-slash');
+        icon.classList.add('fa-eye');
+    }
+}
